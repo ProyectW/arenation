24b19e4349c8152e5ccc6c21c19b4c1491ceb760
Jose Ayazo <josemanuelayazo@gmail.com>
2021-10-14 17:26:53 -0500

    Listo los colores, fonts y tamaños de titulos. Agregué el logo svg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 function App() {
-    return <h1 className="text-4xl font-semibold text-center">Arenation</h1>;
+    return (
+        <>
+            <h1 className="text-4xl font-semibold text-center">Arenation</h1>
+            <h2>Title 2</h2>
+            <h3>Title 3</h3>
+            <h4>Title 4</h4>
+            <h5>Title 5</h5>
+            <p>Esto es un texto</p>
+        </>
+    );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@ module.exports = {
     purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     darkMode: false, // or 'media' or 'class'
     theme: {
+        colors: {
+            "primary-500": "#1045FF",
+            "primary-300": "#217DFF",
+            "primary-100": "#33B0FF",
+            "secondary-dark": "#070928",
+            "secondary-light": "#F8F8FA",
+            "secondary-gray": "#9CA3AF",
+            danger: "#F16063",
+            warning: "#FFB020",
+            success: "#66CB9F",
+        },
+        fontFamily: {
+            noto: ["Noto Sans", "sans-serif"],
+            gilroy: ["Gilroy", "sans-serif"],
+        },
         extend: {},
     },
     variants: {
