20495100638575a3ba7d99a3666e7c823b1132fa
Jose Ayazo <josemanuelayazo@gmail.com>
2021-10-27 17:15:56 -0500

    Terminado el hero, agregado las dos siguientes secciones en el page Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero";
+import {
+    IdealVenueSection,
+    LocalBannerAdsSection,
+} from "../components/HomeSections";
 export default class Home extends React.Component {
     render() {
         return (
             <>
                 <Hero />
+                <IdealVenueSection />
+                <LocalBannerAdsSection />
             </>
         );
     }
