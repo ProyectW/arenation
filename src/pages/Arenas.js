9e1a58fd1b3b1da23b0907c84d3a78722d1bfc6e
Faber Hoyos <faberhoyos01@gmail.com>
2021-11-03 18:39:43 -0500

    Component ArenaHeader

--- src/pages/Arenas.js
+++ src/pages/Arenas.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchResultsHeader from "../components/Controls/SearchResultsHeader";
 import ArenaCard from "../components/ArenaCard";
+import ArenaHeader from "../components/Header/ArenaHeader";
 
 export default class Arenas extends React.Component {
     render() {
@@ -13,7 +14,15 @@ export default class Arenas extends React.Component {
         return (
             <div className="pt-16 pb-44">
                 <div><SearchResultsHeader filter={filterOptions} /></div>
-                <div className="w-full grid grid-cols-4">
+                <div className="px-8 py-8 sm:px-16">
+                    <ArenaHeader
+                        name = "Cancha Nuevo Milenio"
+                        puntuacion = "4.1"
+                        ubicacion = "Montería, Córdoba"
+                        favorito={true}
+                    />
+                </div>
+                <div className="px-8 py-8 sm:px-16 grid grid-cols-4">
                     <ArenaCard favorito={true}/>
                     <ArenaCard favorito={true}/>
                     <ArenaCard favorito={true}/>
