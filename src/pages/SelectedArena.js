cc87084c3af800d8d20904dc27dfe47c35a8a651
Faber Hoyos <faberhoyos01@gmail.com>
2021-11-03 19:04:18 -0500

    cambios en selectarena cambios basicos como de tamaños o padding

--- src/pages/SelectedArena.js
+++ src/pages/SelectedArena.js
@@ -1,9 +1,15 @@
 import React from "react";
+import ArenaHeader from "../components/Header/ArenaHeader";
 
 export default class SelectedArena extends React.Component {
     render() {
         return (
             <div className="pt-16 pb-44">
+                <ArenaHeader
+                    name="Cancha Nuevo Mileneo"
+                    puntuacion="4.5"
+                    ubicacion="Montería, Córdoba"
+                    favorito={true}/>
             </div>
         );
     }
