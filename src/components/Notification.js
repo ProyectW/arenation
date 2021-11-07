683eefb252a2b4723d0b85c4efb615e1959f0047
Faber Hoyos <faberhoyos01@gmail.com>
2021-11-06 22:28:36 -0500

    Notificaciones vacias page agregada

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FechaConDia } from "../utils/Fecha";
 import Logo from "../assets/img/iconLogo.svg";
+import iconEmptyNotifications from "../assets/img/iconEmptyNotifications.svg";
 import Button from "./Controls/Buttons";
 
 export default function Notification(props) {
@@ -42,20 +43,36 @@ export default function Notification(props) {
       fecha: "2021/11/05",
     },
   ];
-  return (
-    <div className="flex flex-col w-full px-8 py-8 sm:px-16 text-secondary-dark">
-      {notifications.map((e) => {
-        return (
-          <div className="py-2">
-            <Button
-              type="infoBtnLogo"
-              children={Logo}
-              label={e.comentario}
-              value={<FechaConDia fecha={e.fecha} />}
-            />
-          </div>
-        );
-      })}
-    </div>
-  );
+
+  if (notifications) {
+    
+    return (
+      <div className="flex flex-col w-full px-8 py-8 sm:px-16 text-secondary-dark">
+        {notifications.map((e) => {
+          return (
+            <div className="py-2">
+              <Button
+                type="infoBtnLogo"
+                children={Logo}
+                label={e.comentario}
+                value={<FechaConDia fecha={e.fecha} />}
+              />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex flex-col w-full px-8 py-8 sm:px-16 text-secondary-dark">
+        <div className="w-full flex flex-col items-center justify-center text-secondary-dark">
+          <img alt="" src={iconEmptyNotifications}/>
+          <h1 className="font-semibold">¡No hay nada aqui!</h1>
+          <label>
+            No te preocupes, te avisaremos cuando tengamos algo nuevo para ti
+          </label>
+        </div>
+      </div>
+    );
+  }
 }
