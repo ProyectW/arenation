9006ec30967c2594a3294ea36d7a333816685376
Faber Hoyos <faberhoyos01@gmail.com>
2021-11-07 03:13:53 -0500

    Pagina vacia de arenas agregada

--- src/pages/Arenas.js
+++ src/pages/Arenas.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchResultsHeader from "../components/Controls/SearchResultsHeader";
 import ArenaCard from "../components/ArenaCard";
+import SearchArena from "../components/SearchArena";
 
 export default class Arenas extends React.Component {
     render() {
@@ -8,21 +9,11 @@ export default class Arenas extends React.Component {
             sport: "Fútbol",
             city: "San Pelayo",
             resultCount: 20,
-        };
-
+        }
         return (
             <div className="pt-16 pb-44">
                 <div><SearchResultsHeader filter={filterOptions} /></div>
-                <div className="px-8 py-8 sm:px-16 grid grid-cols-3 justify-items-center">
-                    <ArenaCard favorito={true}/>
-                    <ArenaCard favorito={true}/>
-                    <ArenaCard favorito={true}/>
-                    <ArenaCard favorito={false}/>
-                    <ArenaCard favorito={false}/>
-                    <ArenaCard favorito={false}/>
-                    <ArenaCard favorito={false}/>
-                    <ArenaCard favorito={false}/>
-                </div>
+                <SearchArena/>
             </div>
         );
     }
